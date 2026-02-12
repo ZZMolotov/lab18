18ff525bf218a1049751048b5de1864e6faa124c
ZZMolotov <zzmolotov8559@gmail.com>
2026-02-12 22:31:58 +0700

    lab18_1 fixed

--- lab18_1.cpp
+++ lab18_1.cpp
@@ -1,12 +1,23 @@
 #include<iostream>
+#include <cmath>
 using namespace std;
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect R1, Rect R2){
+    double xleft  = max(R1.x, R2.x);
+    double xright = min(R1.x+R1.w, R2.x+R2.w);
+    double ytop = min(R1.y, R2.y);
+    double ybottom = max(R1.y-R1.h, R2.y-R2.h);
+    
+    double w = xright - xleft;
+    double h = ytop-ybottom;
+    
+    if(w > 0 && h>0){
+        return w*h;
+    }else {
+        return 0;
+    }
 }
